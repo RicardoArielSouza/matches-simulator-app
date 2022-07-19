31cbdd720c8cd44d4fc43edb971f54a5f09d51e0
Ricardo Ariel Costa Souza <ricardo.devmob@gmail.com>
2022-07-19 12:54:02 -0300

    mcriações e mudanças nos layouts

---
+++ app/src/main/java/me/dio/simulator/DetailActivity.kt
@@ -0,0 +1,20 @@
+package me.dio.simulator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.dio.simulator.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
